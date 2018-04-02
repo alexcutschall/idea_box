0351863ed40351c6490dec7fd6e769e897cc9800
alexcutschall <alexander.cutschall@gmail.com>
2018-04-02 15:14:20 -0600

    Adding admin/default user specs and tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180402201811) do
+ActiveRecord::Schema.define(version: 20180402205918) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.integer "role", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
